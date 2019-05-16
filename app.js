1be2e8f603312f5b710a57be5bc2e997cedd2b8a
= <siheonl@andrew.cmu.edu>
2019-05-16 17:28:04 -0400

    event listeners modified

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const canvas = document.getElementById("jsCanvas");
-
+let painting = false;
+function stopPainting() {
+    painting = false;
+}
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
 } 
 
 function onMouseDown(event) {
-    console.log(event);
+    painting = true;
 }
+
+function onMouseUp(event) {
+    stopPainting();
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseup);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
